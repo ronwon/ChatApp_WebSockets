7529eba90ffd83b33c4688a7c70a02a52ed382ae
ronwon <aggarwal.mayank49@gmail.com>
2019-06-08 21:28:08 +0530

    Modified file

--- public/chat.js
+++ public/chat.js
@@ -11,15 +11,16 @@ var userFormArea = document.getElementById('userFormArea');
 var msgBtn = document.getElementById('msgBtn');
 var msgInput = document.getElementById('msgInput');
 var chatArea = document.getElementById('chat');
-
+var feedback = document.getElementById('feedback');
 
 btn.addEventListener('click', function() {
     event.preventDefault();
     name = inp.value;
-    socket.emit('newUser', {name:inp.value});
-    userFormArea.style.display = 'none';
-    msgArea.style.display = 'block';
-    console.log("You entered:",inp.value);
+        socket.emit('newUser', {name:inp.value});
+        userFormArea.style.display = 'none';
+        msgArea.style.display = 'block';
+        console.log("You entered:",inp.value);
+    
     
 });
 
@@ -69,23 +70,70 @@ socket.on('RecOtherMsg', function(data) {
 });
 
 // HANDLE TYPING RECEIPT
-socket.on('typing', function(data) {
-    console.log(data, 'is typing');
-});
+// socket.on('typing', function(data) {
+//     console.log(data, 'is typing');
+    
+// });
 
-if(data.length == 1 && data[0]==name) {
-    s = '';
-}
-else if(data.length > 0) {
-    s += '<p class="grey-text text-darken-3 lighten-3"><em>';
-    for(let i=0; i<data.length; i++) {
-        if(data[i] != name) {
-            s += data[i];
-            if(i != data.length-1) {
-                s += ', ';
+socket.on('typingUsersUpdate', function(data) {
+    let s = '';
+    // data.splice(data.indexOf(name), 1);
+    /*if(data.length == 1 && data[i]==name) {
+        s = '';
+    }
+    else if(data.length > 0) {
+        s += '<p class="grey-text text-darken-3 lighten-3"><em>';
+        for(let i=0; i<data.length; i++) {
+            if(data[i] != name) {
+                s += data[i];
+                if(i != (data.length-1)){
+                    s += ', ';
+                }
             }
         }
+        if(data.length == 1) {
+            s += ' ';
+        }
+        s += 'is typing</em></p>'
     }
-    s += ' is typing</em></p>';
-}
-feedback.innerHTML = s;
+    feedback.innerHTML = s;*/
+    if(data.length == 1 && data[0]==name) {
+        s = '';
+    }
+    else if(data.length > 0) {
+        let isFirst = true;
+        s += '<p class="grey-text text-darken-3 lighten-3" id="fback"><em>';
+        for(let i=0; i<data.length; i++) {
+            if(data[i] != name) {
+                if(isFirst == false) {
+                    s += ', ';
+                }
+                else {
+                    isFirst = false;
+                }
+                s += data[i];
+            }
+        }
+        s += ' is typing</em></p>';
+    }
+    feedback.innerHTML = s;
+
+
+});
+
+// if(data.length == 1 && data[0]==name) {
+//     s = '';
+// }
+// else if(data.length > 0) {
+//     s += '<p class="grey-text text-darken-3 lighten-3"><em>';
+//     for(let i=0; i<data.length; i++) {
+//         if(data[i] != name) {
+//             s += data[i];
+//             if(i != data.length-1) {
+//                 s += ', ';
+//             }
+//         }
+//     }
+//     s += ' is typing</em></p>';
+// }
+// feedback.innerHTML = s;
